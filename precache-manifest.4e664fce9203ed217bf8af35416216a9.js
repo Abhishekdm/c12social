fb766dfc9f331b22ca938a7d9bbb8bd1f838d75a
Ajmal Shah <ajmalshah@Ajmals-MacBook-Air.local>
2020-10-21 13:08:30 +0530

    Updates

---
+++ precache-manifest.4e664fce9203ed217bf8af35416216a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c7bbf5c5edc6b5e8313921a989470ab",
+    "url": "/c12social/index.html"
+  },
+  {
+    "revision": "24e3c0e2b718dd60f264",
+    "url": "/c12social/static/css/main.2f408a95.chunk.css"
+  },
+  {
+    "revision": "37d697f7d3f30d2315f0",
+    "url": "/c12social/static/js/2.9147c1bf.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/c12social/static/js/2.9147c1bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24e3c0e2b718dd60f264",
+    "url": "/c12social/static/js/main.e8474ca5.chunk.js"
+  },
+  {
+    "revision": "e47d696f37906c150cab",
+    "url": "/c12social/static/js/runtime-main.104df8b9.js"
+  }
+]);
