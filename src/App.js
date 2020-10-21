b2a5a636909d446f642583aa7cfc1377a7fceeec
Ajmal Shah <ajmalshah@Ajmals-MacBook-Air.local>
2020-10-21 13:01:26 +0530

    lst

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import './App.css';
+
+import SignIn from '../src/componets/Authentication/SignIn'
+import SignUp from '../src/componets/Authentication/SignUp'
+import Feeds from '../src/componets/FeedsPage/Feeds'
+ 
+function App() {
+  return (
+    <div className="App">
+     <Router>
+        <Switch>
+          <Route exact path="/" component={SignIn}  />
+          <Route exact path="/sign-up" component={SignUp} />
+          <Route exact path="/feeds" component={Feeds} />
+          {/* <Route path="/feed/:feedId" component={Feed} />
+          <Route path="/user/:userId" component={User} />
+          <Route component={NotFound} /> */}
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
